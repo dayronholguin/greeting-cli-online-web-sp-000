285475e44ceb10ece1d237d0ca41c6cf7998ef23
Dayron Holguin <dayronholguin@yahoo.com>
2019-09-07 04:37:58 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,7 @@
-# code the #greeting method here!
+def greeting(name = 'Programmer')
+  puts "Hi! I'm HAL, what's your name?"
+  puts name = gets.strip
+  puts "Hello #{name.strip}. It's nice to meet you."
+end
+
+greeting
